b4ee6903665d02cc2f7b17db584bf3bbe70d2a4d
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2022-02-16 23:10:06 +0100

    fine crude e validate

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 // Area Privata - Backoffice
-Auth::routes();
 
 Route::prefix("admin")->namespace("Admin")->middleware("auth")->group(function() {
     Route::get('/home', 'HomeController@index')->name('home');
